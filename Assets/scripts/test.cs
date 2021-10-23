1fe8f1a516e86c5f809f81f1062ca7c18b7d6ca7
TheNerkinz <igorekaterinamushta@gmail.com>
2021-10-23 15:35:07 +0300

    23.10.2021

---
+++ Assets/scripts/test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour{
+    public void Start()
+    {
+        int health = 100; 
+        float Health = 0,2f;
+        string name = "bablkvaser";
+
+        health--;
+    }
+}
