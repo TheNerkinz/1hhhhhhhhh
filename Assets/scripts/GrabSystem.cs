5ae3844fce301d71c8f6ba798e1cbabe9e3d457a
TheNerkinz <igorekaterinamushta@gmail.com>
2021-12-11 15:23:24 +0300

    11.12.21 2

---
+++ Assets/scripts/GrabSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GrabSystem : MonoBehaviour
+{
+	public Transform camera;
+
+	private Interactable _interactable;
+	
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			camera.gameObject.SetActive(true);
+			Ray ray = new Ray(camera.position, camera.forward);
+
+			RaycastHit raycastHit;
+			if (Physics.Raycast(ray, out raycastHit, 100))
+			{
+				_interactable = raycastHit.collider.GetComponent<Interactable>();
+				if (_interactable != null)
+				{
+					_interactable.Follow(camera);
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (_interactable != null)
+			{
+				_interactable.Drop();
+				_interactable = null;
+			}
+		} 
+	}
+}
---
+++ Assets/scripts/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Interactable : MonoBehaviour
+{
+    public float speed = 5;
+    
+    public Rigidbody rigidbody;
+    
+    private Transform _playerHead;
+    private bool _isFollow;
+    
+    public void Follow(Transform playerHead)
+    {
+        _playerHead = playerHead;
+        _isFollow = true;
+
+        rigidbody.useGravity = false;
+    }
+
+    public void Drop()
+    {
+        _isFollow = false;
+        rigidbody.useGravity = true;
+    }
+    
+    private void Update()
+    {
+        if (_isFollow == false)
+        {
+            return;
+        }
+
+        var target = _playerHead.position + _playerHead.forward * 3;
+        
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * speed);
+    }
+}
