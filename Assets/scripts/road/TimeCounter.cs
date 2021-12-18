227fb28764bfcf020fef87fa762dff981a72be31
TheNerkinz <igorekaterinamushta@gmail.com>
2021-12-18 15:29:51 +0300

    18.12.2021

---
+++ Assets/scripts/road/TimeCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour {
+
+    public Text minutes;
+    public Text seconds;
+
+
+    public TimeWiew timewiew;
+
+   
+}
---
+++ Assets/scripts/road/TimeWiew.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWiew : MonoBehaviour {
+
+   private float currentTime = 0f;
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+
+    }
+    public int GetMinutes()
+    {
+        return (int)currentTime;
+    }
+
+}
